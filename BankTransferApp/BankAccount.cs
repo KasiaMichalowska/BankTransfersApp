760cfc151ff80118f1b257312ae3196f534d5500
Katarzyna Michalowska <kami@powel.com>
2020-11-16 09:29:02 +0100

    homework

---
+++ BankTransferApp/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BankTransfers
+{
+    public class BankAccount
+    {
+        private readonly string _accountName;
+        private readonly Guid _accountNumber;
+        private decimal _accountBalance;
+
+        public BankAccount(string name)
+        {
+            _accountName = name;
+            _accountNumber = Guid.NewGuid();
+            _accountBalance = 1000;
+        }
+
+        public decimal AccountBalance
+        {
+            get => _accountBalance;
+            set => _accountBalance = value;
+        }
+
+        public string Name => _accountName;
+
+        public Guid AccountNumber => _accountNumber;
+
+        public override string ToString()
+        {
+            return $"   Account: {_accountName}\n" +
+                   $"   Account No: {_accountNumber}\n" +
+                   $"   Balance: ${_accountBalance}\n";
+        }
+    }
+}
